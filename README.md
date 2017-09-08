c460e7dd05ed79f7ee51fd34abc0fa4945527f5e
Trey Shugart <treshugart@gmail.com>
2017-09-08 15:30:40 +1000

    Update docs and remove input() for the js() format.

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ npm install conartist
 
 ## Usage
 
-conartist works off of a single source of truth. This is in the form of a `conartist.js` config file. This file exports functions for each file it will manage. For example:
+Conartist works off of a single source of truth. This is in the form of a `conartist.js` config file. This file exports a formatter for each file it needs to manage. The formats are defined by calling a function and passing it a function that returns the configuration for the specified format.
 
 ```js
 const { json } = require('conartist');
@@ -36,6 +36,38 @@ As shown, each function must implement a particular format so conartist knows ho
 When you run the `conartist` command, it will output a file for each config path you specify in the `exports`. For the above configuration, it will output both a `.babelrc` and a `package.json`.
 
 Notice that both JSON files are using the `input()` function. This function returns any existing configuration for the specified format so that you can merge it with your defaults. This is a particularly powerful point because you can not only scaffold out a project, but also keep it in sync with any modifications you make.
+
+### Formats
+
+Formats are used by calling the format as a function and passing it a function that returns your configuration. For example:
+
+```js
+const { js } = require('conartist');
+
+module.exports = {
+  'package.json': js((file, input) => {
+
+  })
+};
+```
+
+The `file` argument is the filename you've specified. This is useful if you're not defining your formatter inline and the fucntion needs to somehow know the name of the file.
+
+The `input` argument is a function that will return the existing configuration. You can use this to merge the existing state with whatever you're doing in your formatter.
+
+#### `js`
+
+The `js` format allows you to write a configuration using standard JavaScript. The formatter will output a file that points to the JavaScript you've written in your `conartist.js` file, as opposed to trying to stringify it. This means any stuff you use from the outer scope is still available to your config.
+
+*Calling `input()` to get the file from dist doesn't make sense here because it points to itself, so it just returns `null` if you try it.*
+
+#### `json`
+
+The `json` format allows you to return a JavaScript object and it will try and conver it to JSON and write it to the corresponding file. The `input` function will give you a JavaScript object (it simply requires the current JSON file) so you can merge it however you see fit with the object you are returning.
+
+#### `string`
+
+The `string` format allows you to return a string and it will write it as-is to the file. The `input` function returns you a string, so if you're going to merge the configs it is up to you to intelligently manage this.
 
 ### Presets
 
@@ -73,3 +105,26 @@ const userConfig = merge({
 ```
 
 This allows you to switch features on or off. Your base config can provide safe defaults for everything, and extensions to it can decide what it wants to take from it quite easily.
+
+### Custom formatters
+
+You can write a custom formatter if you need to.
+
+```js
+const { createFormat } = require('conartist');
+
+const myFormat = createFormat(class {
+  input(file) {
+
+  }
+  output(file, merge) {
+
+  }
+});
+```
+
+You optionally define an `input()` function that takes the file being handled. It's responsibility is to take the current file contents and returned the transformed result. For example, in the `json` formatter we `require()` the file and return the result.
+
+If you do not define `input()`, or the file does not exist, calling `input()` from your formatter function will return `null`.
+
+The `output()` function is responsible for taking the `file` it's handling and returning the contents of the file that you want to write to disk. The `merge()` function will return the result of your fomatting function and you're responsible for transforming this to a string that can be saved to a file. For example, the `json` format will `JSON.stringify()` the result of this.
--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-const { merge } = require('lodash');
-const fs = require('fs');
-const outdent = require('outdent');
-const path = require('path');
-const prettier = require('prettier');
-const util = require('util');
+const { merge } = require("lodash");
+const fs = require("fs");
+const outdent = require("outdent");
+const path = require("path");
+const prettier = require("prettier");
+const util = require("util");
 
 const asyncFileExists = util.promisify(fs.exists);
 const asyncReadFile = util.promisify(fs.readFile);
 
-class JsOrJson {
-  input(file) {
-    return require(`./${file}`);
-  }
-}
-
 const js = createFormat(
-  class extends JsOrJson {
+  class {
     output(file) {
       return prettier.format(
         outdent`
@@ -23,17 +17,20 @@ const js = createFormat(
           const key = '${file}';
           module.exports = require('./conartist.js')[key].merge(key);
         `,
-        { parser: 'babylon' }
+        { parser: "babylon" }
       );
     }
   }
 );
 
 const json = createFormat(
-  class extends JsOrJson {
+  class {
+    input(file) {
+      return require(`./${file}`);
+    }
     output(file, merge) {
       return prettier.format(JSON.stringify(merge()), {
-        parser: 'json'
+        parser: "json"
       });
     }
   }
@@ -45,7 +42,7 @@ const string = createFormat(
       return asyncReadFile(file);
     }
     output(file, merge) {
-      return merge();
+      return `${merge()}`;
     }
   }
 );
@@ -71,7 +68,7 @@ function createFormat(Base) {
 }
 
 const config = {
-  '.editorconfig': string(
+  ".editorconfig": string(
     () => outdent`
       root = true
 
@@ -84,7 +81,7 @@ const config = {
       insert_final_newline = true
     `
   ),
-  '.flowconfig': string(
+  ".flowconfig": string(
     () => outdent`
       [ignore]
       .*/\..*
@@ -105,7 +102,7 @@ const config = {
       unsafe.enable_getters_and_setters=true
     `
   ),
-  '.gitignore': string(
+  ".gitignore": string(
     () => outdent`
       /coverage
       /demo/dist
@@ -120,99 +117,99 @@ const config = {
       npm-debug.log*
     `
   ),
-  '.nvmrc': string(() => '7.9.0'),
-  '.travis.yml': string(() => 'language: node_js'),
-  'package.json': json((file, input) =>
+  ".nvmrc": string(() => "7.9.0"),
+  ".travis.yml": string(() => "language: node_js"),
+  "package.json": json((file, input) =>
     merge(
       {
         dependencies: {
-          '@skatejs/bore': '4.0.0',
-          '@skatejs/ssr': '0.12.2',
-          '@skatejs/val': '0.3.1',
-          'babel-cli': '6.24.1',
-          'babel-eslint': '7.2.3',
-          'babel-plugin-modules-map': '1.0.0',
-          'babel-plugin-modules-web-compat': '1.1.1',
-          'babel-preset-env': '1.6.0',
-          'babel-preset-es2015-rollup': '3.0.0',
-          'babel-preset-latest': '6.24.1',
-          'babel-preset-react': '6.24.1',
-          'babel-preset-stage-0': '6.24.1',
-          commitizen: '2.9.6',
-          'cz-conventional-changelog': '2.0.0',
-          'eslint-plugin-flowtype': '2.34.0',
-          'get-typed': '1.0.0-beta.1',
-          'highlight.js': '9.12.0',
-          husky: '0.13.3',
-          jest: '20.0.4',
-          lodash: '4.17.4',
-          'lint-staged': '4.0.2',
-          nodemon: '1.11.0',
-          outdent: '*',
-          preact: '*',
-          rollup: '0.47.4',
-          'rollup-plugin-babel': '3.0.2',
-          'rollup-plugin-uglify': '2.0.1',
-          'semantic-release': '6.3.2',
-          typescript: '2.3.3',
-          'typescript-formatter': '5.2.0',
-          'validate-commit-msg': '2.12.1',
-          watchman: '0.1.8',
-          yargs: '8.0.1'
+          "@skatejs/bore": "4.0.0",
+          "@skatejs/ssr": "0.12.2",
+          "@skatejs/val": "0.3.1",
+          "babel-cli": "6.24.1",
+          "babel-eslint": "7.2.3",
+          "babel-plugin-modules-map": "1.0.0",
+          "babel-plugin-modules-web-compat": "1.1.1",
+          "babel-preset-env": "1.6.0",
+          "babel-preset-es2015-rollup": "3.0.0",
+          "babel-preset-latest": "6.24.1",
+          "babel-preset-react": "6.24.1",
+          "babel-preset-stage-0": "6.24.1",
+          commitizen: "2.9.6",
+          "cz-conventional-changelog": "2.0.0",
+          "eslint-plugin-flowtype": "2.34.0",
+          "get-typed": "1.0.0-beta.1",
+          "highlight.js": "9.12.0",
+          husky: "0.13.3",
+          jest: "20.0.4",
+          lodash: "4.17.4",
+          "lint-staged": "4.0.2",
+          nodemon: "1.11.0",
+          outdent: "*",
+          preact: "*",
+          rollup: "0.47.4",
+          "rollup-plugin-babel": "3.0.2",
+          "rollup-plugin-uglify": "2.0.1",
+          "semantic-release": "6.3.2",
+          typescript: "2.3.3",
+          "typescript-formatter": "5.2.0",
+          "validate-commit-msg": "2.12.1",
+          watchman: "0.1.8",
+          yargs: "8.0.1"
         },
         jest: {
-          modulePathIgnorePatterns: ['./node_modules'],
-          setupFiles: ['./test/unit/setup'],
-          setupTestFrameworkScriptFile: './test/unit/setup-each',
-          testEnvironment: '@skatejs/ssr/jest'
+          modulePathIgnorePatterns: ["./node_modules"],
+          setupFiles: ["./test/unit/setup"],
+          setupTestFrameworkScriptFile: "./test/unit/setup-each",
+          testEnvironment: "@skatejs/ssr/jest"
         },
-        'lint-staged': {
-          '*.(js|json)': ['prettier --write', 'git add']
+        "lint-staged": {
+          "*.(js|json)": ["prettier --write", "git add"]
         },
         scripts: {
-          postinstall: 'conartist'
+          postinstall: "conartist"
         }
       },
       input()
     )
   ),
-  'rollup.config.js': js(() => {
-    const babel = require('rollup-plugin-babel');
-    const uglify = require('rollup-plugin-uglify');
-    const yargs = require('yargs');
+  "rollup.config.js": js(() => {
+    const babel = require("rollup-plugin-babel");
+    const uglify = require("rollup-plugin-uglify");
+    const yargs = require("yargs");
     const args = yargs.argv;
     return {
-      dest: `umd/index${args.min ? '.min' : ''}.js`,
-      entry: 'src/index.js',
-      external: ['preact'],
-      format: 'umd',
-      globals: { preact: 'preact' },
-      moduleName: 'skate',
-      plugins: [babel(require('./config/babel.umd'))].concat(
+      dest: `umd/index${args.min ? ".min" : ""}.js`,
+      entry: "src/index.js",
+      external: ["preact"],
+      format: "umd",
+      globals: { preact: "preact" },
+      moduleName: "skate",
+      plugins: [babel(require("./config/babel.umd"))].concat(
         args.min ? uglify() : []
       ),
       sourceMap: true
     };
   }),
-  'tsconfig.json': json((file, input) =>
+  "tsconfig.json": json((file, input) =>
     merge(
       {
         compilerOptions: {
-          module: 'es2015',
-          target: 'es2017',
-          lib: ['dom', 'es2017'],
-          baseUrl: './',
+          module: "es2015",
+          target: "es2017",
+          lib: ["dom", "es2017"],
+          baseUrl: "./",
           strict: true,
           sourceMap: true,
-          moduleResolution: 'node',
+          moduleResolution: "node",
           declaration: true,
-          jsx: 'react',
-          jsxFactory: 'h',
+          jsx: "react",
+          jsxFactory: "h",
           pretty: true,
-          outDir: 'dist'
+          outDir: "dist"
         },
-        include: ['src/index.ts'],
-        exclude: ['node_modules']
+        include: ["src/index.ts"],
+        exclude: ["node_modules"]
       },
       input()
     )
