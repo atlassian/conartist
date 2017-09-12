eb3ffc97b2a987637345c571137b4e665bc95251
Trey Shugart <treshugart@gmail.com>
2017-09-12 14:13:33 +1000

    Fix paths in bin entry.

--- src/bin/index.js
+++ src/bin/index.js
@@ -7,8 +7,8 @@ const yargs = require('yargs');
 const { argv } = yargs;
 const cmd = argv._.join(path.delimiter) || 'sync';
 
-if (fs.existsSync(path.join(__dirname, '..', 'node', 'bin', `${cmd}.js`))) {
-  require(`../node/bin/${cmd}`)(argv);
+if (fs.existsSync(path.join(__dirname, `${cmd}.js`))) {
+  require(`./${cmd}`)(argv);
 } else {
   throw new Error(`Command not found: ${cmd}`);
 }
