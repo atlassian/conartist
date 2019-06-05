e5c60181152df1e32f6a9b317b3c5c08ac7ccdf6
Trey Shugart <treshugart@gmail.com>
2019-06-05 20:44:32 +1000

    Move bin and update error message.

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,15 @@
 #! /usr/bin/env node
 
 const cosmiconfig = require("cosmiconfig");
-const pkg = require("./package.json");
-const { bin } = require("./src");
+const pkg = require("../package.json");
+const { bin } = require("../src");
 
 (async function main() {
   const search = await cosmiconfig("conartist").search();
   if (!search && process.argv.indexOf("--help") === -1) {
-    console.error('No "conartist" cosmiconfig file found.');
+    console.error(
+      "No conartist configuration file found. For more information see https://github.com/treshugart/conartist#install for ways you can configure conartist."
+    );
     process.exit(1);
   }
   await bin({
