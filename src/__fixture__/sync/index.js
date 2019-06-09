39df101587baba9c7d407786dfd39c18e09ee7cb
Trey Shugart <treshugart@gmail.com>
2019-06-09 12:03:32 +1000

    Add test for removing.

--- src/__fixture__/sync/index.js
+++ src/__fixture__/sync/index.js
@@ -13,6 +13,10 @@ module.exports = {
       name: "file2",
       data: { test: "index -> file2" },
       overwrite: false
+    },
+    {
+      name: "file3",
+      remove: true
     }
   ],
   include: ["./src/__fixture__/sync/include1", require("./include2")]
